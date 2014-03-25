77277008effe621489a3c17aff727e7f0146565f
Tim <Tim@Tim-PC>
2014-03-25 08:44:47 -0500

    fixed a problem with myAdapter being pulled

--- sched/src/main/java/com/cal/sched/myAdapter.java
+++ sched/src/main/java/com/cal/sched/myAdapter.java
@@ -8,7 +8,6 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 
 /**
  * Created by Tim on 2/12/14.
@@ -16,41 +15,25 @@ import java.util.HashMap;
 public class myAdapter extends BaseAdapter
 {
     Context context;
-<<<<<<< HEAD
-    HashMap<String, String> listers = new HashMap<>();
-    private static LayoutInflater inflater = null;
-
-    public myAdapter(Context context, HashMap<String, String> listers)
-=======
     String[] classes;
     String[] teachers;
     private static LayoutInflater inflater = null;
 
     public myAdapter(Context context, String[] classes, String[] teachers)
->>>>>>> revised_main
     {
         this.context = context;
-        this.listers = listers;
+        this.classes = classes;
+        this.teachers = teachers;
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
     public int getCount() {
-<<<<<<< HEAD
-        return listers.size();
-    }
-
-    @Override
-    public Object getItem(int i) {
-        return listers.get(i);
-    }
-=======
         return classes.length;
     }
 
     @Override
     public Object getItem(int i) { return classes[i] + " " + teachers[i]; }
->>>>>>> revised_main
 
     @Override
     public long getItemId(int i) {
@@ -61,12 +44,6 @@ public class myAdapter extends BaseAdapter
     public View getView(int i, View convertView, ViewGroup viewGroup)
     {
         View myView = convertView;
-<<<<<<< HEAD
-        TextView classes = (TextView) findViewById(R.id.aclass);
-        TextView teachers = (TextView) findViewById(R.id.ateacher);
-        classes = listers.get(i);
-        teachers = listers.get(i);
-=======
         if(myView == null)
             myView = inflater.inflate(R.layout.lin_class, null);
         TextView tclass = (TextView) myView.findViewById(R.id.aclass);
@@ -74,7 +51,6 @@ public class myAdapter extends BaseAdapter
         tclass.setText(classes[i]);
         tteacher.setText(teachers[i]);
 
->>>>>>> revised_main
         return myView;
     }
 }
