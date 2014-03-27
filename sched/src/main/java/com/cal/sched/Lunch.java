ababe7147f429f7d0c9319832ce3b5b3c3f095e4
trumpetboy585 <trumpetboy585@gmail.com>
2014-03-27 06:47:42 -0500

    added ToggleButton for all 10 lunch options that I have.  I just have to fix what happens inside of the if-else statements and hopefully everything will work out  properly and things will work as expected

--- sched/src/main/java/com/cal/sched/Lunch.java
+++ sched/src/main/java/com/cal/sched/Lunch.java
@@ -10,19 +10,172 @@ import android.widget.CompoundButton;
 import android.widget.ToggleButton;
 
 
-public class Lunch extends ActionBarActivity {
+public class Lunch extends ActionBarActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lunch_info);
 
-        ToggleButton c100 = (ToggleButton) findViewById(R.id.b_lunch100);
-        c100.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isToggled) {
-                if (isToggled) {
+        //B LUNCH
+        ToggleButton b100 = (ToggleButton) findViewById(R.id.b_lunch100);
+        b100.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isToggled)
+            {
+                if (isToggled)
+                {
                     Log.i("info", "Button2 is on!");
-                } else {
+                }
+                else
+                {
+                    Log.i("info", "Button2 is off!");
+                }
+            }
+        });
+
+        ToggleButton b78 = (ToggleButton) findViewById(R.id.b_lunch78);
+        b78.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isToggled)
+            {
+                if (isToggled)
+                {
+                    Log.i("info", "Button2 is on!");
+                }
+                else
+                {
+                    Log.i("info", "Button2 is off!");
+                }
+            }
+        });
+
+        ToggleButton b56 = (ToggleButton) findViewById(R.id.b_lunch56);
+        b56.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isToggled)
+            {
+                if (isToggled)
+                {
+                    Log.i("info", "Button2 is on!");
+                }
+                else
+                {
+                    Log.i("info", "Button2 is off!");
+                }
+            }
+        });
+
+        ToggleButton b34 = (ToggleButton) findViewById(R.id.b_lunch34);
+        b34.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isToggled)
+            {
+                if (isToggled)
+                {
+                    Log.i("info", "Button2 is on!");
+                }
+                else
+                {
+                    Log.i("info", "Button2 is off!");
+                }
+            }
+        });
+
+        ToggleButton b12 = (ToggleButton) findViewById(R.id.b_lunch12);
+        b12.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isToggled)
+            {
+                if (isToggled)
+                {
+                    Log.i("info", "Button2 is on!");
+                }
+                else
+                {
+                    Log.i("info", "Button2 is off!");
+                }
+            }
+        });
+
+        //B LUNCH
+        ToggleButton d100 = (ToggleButton) findViewById(R.id.d_lunch100);
+        d100.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isToggled)
+            {
+                if (isToggled)
+                {
+                    Log.i("info", "Button2 is on!");
+                }
+                else
+                {
+                    Log.i("info", "Button2 is off!");
+                }
+            }
+        });
+
+        ToggleButton d78 = (ToggleButton) findViewById(R.id.d_lunch78);
+        d78.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isToggled)
+            {
+                if (isToggled)
+                {
+                    Log.i("info", "Button2 is on!");
+                }
+                else
+                {
+                    Log.i("info", "Button2 is off!");
+                }
+            }
+        });
+
+        ToggleButton d56 = (ToggleButton) findViewById(R.id.d_lunch56);
+        d56.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isToggled)
+            {
+                if (isToggled)
+                {
+                    Log.i("info", "Button2 is on!");
+                }
+                else
+                {
+                    Log.i("info", "Button2 is off!");
+                }
+            }
+        });
+
+        ToggleButton d34 = (ToggleButton) findViewById(R.id.d_lunch34);
+        d34.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isToggled)
+            {
+                if (isToggled)
+                {
+                    Log.i("info", "Button2 is on!");
+                }
+                else
+                {
+                    Log.i("info", "Button2 is off!");
+                }
+            }
+        });
+
+        ToggleButton d12 = (ToggleButton) findViewById(R.id.d_lunch12);
+        d12.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isToggled)
+            {
+                if (isToggled)
+                {
+                    Log.i("info", "Button2 is on!");
+                }
+                else
+                {
                     Log.i("info", "Button2 is off!");
                 }
             }
